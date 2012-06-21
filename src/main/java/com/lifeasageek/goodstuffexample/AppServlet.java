31a8943fe332aa4e1302114435bcd0bb11d98c7b
Justin Wyer <justin@lifeasageek.com>
2012-06-21 14:52:20 +0200

    Part 1 - Jetty & Weld. CDI container available. CDI Bean, Injection & Interceptor example.

---
+++ src/main/java/com/lifeasageek/goodstuffexample/AppServlet.java
@@ -0,0 +1,57 @@
+package com.lifeasageek.goodstuffexample;
+
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Justin Wyer <justin@lifeasageek.com>
+ */
+public class AppServlet implements Servlet
+{
+   Logger log = LoggerFactory.getLogger(AppServlet.class);
+   
+   @Inject
+   private GoodStuff goodStuff;
+
+   @Override
+   public void init(ServletConfig sc) throws ServletException
+   {
+      try
+      {
+         App.getServer().join();
+      }
+      catch (InterruptedException ex)
+      {
+         log.error(ex.getMessage(), ex);
+      }
+      
+      // Lets do something good!
+      goodStuff.doSomethingGood();
+   }
+
+   @Override
+   public ServletConfig getServletConfig()
+   {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   @Override
+   public void service(ServletRequest sr, ServletResponse sr1) throws ServletException, IOException
+   {
+   }
+
+   @Override
+   public String getServletInfo()
+   {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
+   @Override
+   public void destroy()
+   {
+   }
+}
